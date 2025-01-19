e6d2dbea5bfeb919a03aacb9fdb3eabb160a8333
GouravAgarwal06 <gka98967@gmail.com>
2025-01-19 23:35:01 +0530

    changes

--- src/middleware.ts
+++ src/middleware.ts
@@ -6,29 +6,39 @@ const isPublicRoute = createRouteMatcher(['/site', '/api/uploadthing','/agency',
 
 export default clerkMiddleware(async (auth, request) => {
   const url = new URL(request.url);
+  console.log('Clerk Middleware URL:', url);
   const hostname = request.headers.get('host'); // Get the full hostname
+  console.log('Clerk Middleware Hostname:', hostname);
   const domain = process.env.NEXT_PUBLIC_DOMAIN || ''; // Ensure a fallback for the domain
+  console.log('Clerk Middleware Domain:', domain);
   const subdomain = hostname?.split(domain)?.[0]?.replace(/\.$/, ''); // Extract subdomain
+  console.log('Clerk Middleware Subdomain:', subdomain);
 
   // Redirect "/" to "/site"
   if (url.pathname === '/') {
+    console.log("url pathname: ",url.pathname)
     return NextResponse.redirect(new URL('/site', request.url));
   }
 
   // Check if the route is public
   if (isPublicRoute(request)) {
+    console.log('Clerk Middleware Public Route:', request.url);
     return NextResponse.next();
   }
 
   // Handle subdomain-specific logic
   if (subdomain && subdomain !== 'www') {
+    console.log('Clerk Middleware Subdomain inside if statement:', subdomain);
     const searchParams = url.searchParams.toString();
+    console.log('Clerk Middleware Search Params:', searchParams)
     const pathWithSearchParams = `${url.pathname}${searchParams ? `?${searchParams}` : ''}`;
+    console.log('Clerk Middleware Path with Search Params:', pathWithSearchParams)
     return NextResponse.rewrite(new URL(`/${subdomain}${pathWithSearchParams}`, request.url));
   }
 
   // Protect non-public routes
   try {
+    console.log('i am come above auth protect');
     await auth.protect(); // Ensures the user is authenticated
   } catch (error) {
     console.log('Clerk Middleware Error:', error);
